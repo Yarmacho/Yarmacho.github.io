887a4d9260c6693bd641b873fa458f189ce83a09
Yarmacho <65123338+Yarmacho@users.noreply.github.com>
2021-03-24 17:44:00 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function enter(elem){
+	elem.firstElementChild.className = 'speciality__card__filpped';
+}
+
+function exit(elem){
+	elem.firstElementChild.className = 'speciality__card__unfilpped';
+}
